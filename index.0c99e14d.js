aa9fac58bc10cbb1ddb46f10af3ad3244bf73ed8
snackli17 <87991354+snackli17@users.noreply.github.com>
2023-02-08 18:28:35 +0000

    Deploying to gh-pages from @ snackli17/-cybercats_icecream@6a13a2c831dab5f4c3ca0911241cf84a1b467c3e 🚀

---
+++ index.0c99e14d.js
@@ -0,0 +1,2 @@
+var swiperGallary=new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",clickable:!0},loop:!0,scrollbar:{el:".swiper-scrollbar",draggable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}}),swiperReviews=new Swiper(".newSwiper",{pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.0c99e14d.js.map
